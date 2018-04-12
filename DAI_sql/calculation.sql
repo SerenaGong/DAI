83ff6f138abd0ffe95cb7854fe46d4e9f2ea6ae2
Michael Wilson <michael.wilson@odsdai.netdai.com>
2018-04-12 12:03:13 -0700

    Indented queries.

--- DAI_sql/calculation.sql
+++ DAI_sql/calculation.sql
@@ -1,3 +1,26 @@
-select account_id, plan_id, line,lineInfo, last_name, first_name, address_1,address_2,city,state,postal_code, foundation_id,joined_at,prev_balance,adjustments,sum(messages_used) as messages_used , sum(mins_used) as mins_used , sum(data_used) as data_used from account_line_txn_type_agg
-where account_id = 1003574397952
-group by account_id, plan_id, line, lineInfo ,last_name, first_name, address_1,address_2,city,state,postal_code, foundation_id,joined_at,prev_balance,adjustments
+select 
+    account_id, 
+    plan_id, 
+    line,
+    lineInfo, 
+    last_name, 
+    first_name, 
+    address_1,
+    address_2,
+    city,
+    state,
+    postal_code, 
+    foundation_id,
+    joined_at,
+    prev_balance,
+    adjustments,
+    sum(messages_used) as messages_used, 
+    sum(mins_used) as mins_used, 
+    sum(data_used) as data_used 
+from 
+    account_line_txn_type_agg
+where 
+        1=1
+    and account_id = 1003574397952
+group by 
+    account_id, plan_id, line, lineInfo ,last_name, first_name, address_1,address_2,city,state,postal_code, foundation_id,joined_at,prev_balance,adjustments
